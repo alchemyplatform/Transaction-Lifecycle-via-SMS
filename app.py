a63c39ee2fe89e9483c0307d0d00c9b425918942
isaaclau <isaaclau@mit.edu>
2021-07-28 17:48:09 -0700

    app changes

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def request_handler():
                 blockNum =  data['activity'][i]['blockNum']
                 hash =  data['activity'][i]['hash']
 
-        message = client.messages.create(body="TRANSACTION MINED! From: " + from_address + " -> to: " + to_address + " @#:" + blockNum + " CHECK HERE- https://rinkeby.etherscan.io/tx/" +hash ,from_='+14435267244', to='+14158130071')
+        message = client.messages.create(body=" \n TRANSACTION MINED! \n From: " + from_address + " \n To: " + to_address + " \n @#:" + blockNum + " \n CHECK HERE- https://rinkeby.etherscan.io/tx/" +hash ,from_='+14435267244', to='+14158130071')
         print(message.sid)
 
     return ("Ok")
