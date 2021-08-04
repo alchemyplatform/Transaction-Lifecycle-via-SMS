30bdb072e572ef748d90d550e0944e7ca0633f84
isaaclau <isaaclau@mit.edu>
2021-08-04 16:44:57 -0700

    edits

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     include_package_data=False,
     zip_safe=False,
-    install_requires=['Flask','twilio', 'websocket-client','rd'],
+    install_requires=['Flask','twilio', 'websocket-client','rd', 'pickle'],
     entry_points={
         'console_scripts': [
             'runwebhook = app:run',
